91b45222cc9a94a3f9d7042df0d588231f698eb4
Frontend JavaScript <frontendjavascript@iMac-06.local>
2024-03-28 20:44:42 +0200

    curs6

--- cvtemplate/src/index.js
+++ cvtemplate/src/index.js
@@ -1,15 +1,16 @@
 // Import our custom CSS
-import './scss/styles.scss'
+import './scss/styles.scss';
 
 // Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
 
-import particles from 'particles.js'
-import './js/particles.js'
+import particles from 'particles.js';
+import './js/particles.js';
 
-import '../node_modules/typed.js/dist/typed.umd.js'
+import '../node_modules/typed.js/dist/typed.umd.js';
 
-import './js/validare.js'
-import './js/main.js'
-import './js/skill.js'
-import './js/githubapi.js'
+import './js/validare.js';
+import './js/main.js';
+import './js/skill.js';
+import './js/githubapi.js';
+import './js/translate.js';
