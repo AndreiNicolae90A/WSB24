1cc792117d92a0a68ed0fe596e1f15cca9496db5
Frontend JavaScript <frontendjavascript@iMac-06.local>
2024-04-03 20:43:39 +0300

    cvtemp

--- cvtemplate/src/index.js
+++ cvtemplate/src/index.js
@@ -10,7 +10,7 @@ import './js/particles.js';
 import '../node_modules/typed.js/dist/typed.umd.js';
 
 import './js/validare.js';
+import './js/translate.js';
 import './js/main.js';
 import './js/skill.js';
 import './js/githubapi.js';
-import './js/translate.js';
